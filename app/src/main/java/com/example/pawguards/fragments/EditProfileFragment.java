70e69f34a8adf327938ab2aac4f32f272c0229b6
Emirhan Akıtürk <86914272+Weasyls@users.noreply.github.com>
2023-12-30 14:59:35 +0300

    edit profile progress

--- app/src/main/java/com/example/pawguards/fragments/EditProfileFragment.java
+++ app/src/main/java/com/example/pawguards/fragments/EditProfileFragment.java
@@ -7,18 +7,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.pawguards.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EditProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class EditProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private EditText etNameSurname;
+    private EditText etEmail;
+    private EditText etDateOfBirth;
+    private EditText etPassword;
+    private EditText etLocation;
+    private ImageView ivProfilePicture;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -30,15 +31,15 @@ public class EditProfileFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EditProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+    public void init() {
+        etNameSurname = getView().findViewById(R.id.etNameSurname);
+        //etEmail = getView().findViewById(R.id.etEmail);
+        //etDateOfBirth = getView().findViewById(R.id.etDateOfBirth);
+        //etPassword = getView().findViewById(R.id.etPassword);
+        etLocation = getView().findViewById(R.id.etLocation);
+        ivProfilePicture = getView().findViewById(R.id.ivProfilePicture);
+    }
+
     public static EditProfileFragment newInstance(String param1, String param2) {
         EditProfileFragment fragment = new EditProfileFragment();
         Bundle args = new Bundle();
@@ -63,4 +64,12 @@ public class EditProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_edit_profile, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init();
+    }
+
+
 }
